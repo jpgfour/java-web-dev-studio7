2cf4dcb50b7fb769de7db9ed33588311392ef2fe
jpgfour <jeremy.grise@gmail.com>
2020-05-14 20:15:49 -0500

    Generated Abstract Class

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,94 @@
+package org.launchcode.studio7;
+
+public abstract class BaseDisc {
+
+    int storageCapacityInMB;
+    boolean rewriteable;
+    boolean playableByCDDevice;
+    boolean playableByDVDDevice;
+    int spinSpeedLowRangeRPM;
+    int spinSpeedHighRangeRPM;
+    String manufacturer;
+    String id;
+
+    public BaseDisc(int storageCapacityInMB, boolean rewriteable, boolean playableByCDDevice, boolean playableByDVDDevice, int spinSpeedLowRangeRPM, int spinSpeedHighRangeRPM, String manufacturer, String id) {
+        this.storageCapacityInMB = storageCapacityInMB;
+        this.rewriteable = rewriteable;
+        this.playableByCDDevice = playableByCDDevice;
+        this.playableByDVDDevice = playableByDVDDevice;
+        this.spinSpeedLowRangeRPM = spinSpeedLowRangeRPM;
+        this.spinSpeedHighRangeRPM = spinSpeedHighRangeRPM;
+        this.manufacturer = manufacturer;
+        this.id = id;
+    }
+
+    public BaseDisc(int storageCapacityInMB, boolean rewriteable, String id) {
+        this.storageCapacityInMB = storageCapacityInMB;
+        this.rewriteable = rewriteable;
+        this.id = id;
+    }
+
+    public int getStorageCapacityInMB() {
+        return storageCapacityInMB;
+    }
+
+    public void setStorageCapacityInMB(int storageCapacityInMB) {
+        this.storageCapacityInMB = storageCapacityInMB;
+    }
+
+    public boolean isRewriteable() {
+        return rewriteable;
+    }
+
+    public void setRewriteable(boolean rewriteable) {
+        this.rewriteable = rewriteable;
+    }
+
+    public boolean isPlayableByCDDevice() {
+        return playableByCDDevice;
+    }
+
+    public void setPlayableByCDDevice(boolean playableByCDDevice) {
+        this.playableByCDDevice = playableByCDDevice;
+    }
+
+    public boolean isPlayableByDVDDevice() {
+        return playableByDVDDevice;
+    }
+
+    public void setPlayableByDVDDevice(boolean playableByDVDDevice) {
+        this.playableByDVDDevice = playableByDVDDevice;
+    }
+
+    public int getSpinSpeedLowRangeRPM() {
+        return spinSpeedLowRangeRPM;
+    }
+
+    public void setSpinSpeedLowRangeRPM(int spinSpeedLowRangeRPM) {
+        this.spinSpeedLowRangeRPM = spinSpeedLowRangeRPM;
+    }
+
+    public int getSpinSpeedHighRangeRPM() {
+        return spinSpeedHighRangeRPM;
+    }
+
+    public void setSpinSpeedHighRangeRPM(int spinSpeedHighRangeRPM) {
+        this.spinSpeedHighRangeRPM = spinSpeedHighRangeRPM;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/org/launchcode/studio7/OpticalDisc.java
@@ -0,0 +1,20 @@
+package org.launchcode.studio7;
+
+public interface OpticalDisc {
+
+    //decided against using fields in interface
+//    static final boolean needsMediaPlayer = true;
+//    static final boolean worksInDVDPlayer = true;
+
+    void spinDisc();
+    //void readData();
+    //void writeData{};
+    void reportOnLoading();
+    void breakDisc();
+    void coasterizeDisc();
+    void playMedia();
+
+
+
+
+}
